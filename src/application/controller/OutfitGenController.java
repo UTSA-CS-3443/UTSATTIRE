8e58b0967457f9872d886200582deb02947e4696
SethGreco <eug119@my.utsa.edu>
2018-04-10 22:30:33 -0500

    hdh

--- src/application/controller/OutfitGenController.java
+++ src/application/controller/OutfitGenController.java
@@ -9,7 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-
+//yup
 public class OutfitGenController {
 	@FXML
 	public Button btnHome;	
