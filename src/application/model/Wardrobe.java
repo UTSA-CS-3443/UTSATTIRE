72861ca606ca27006fe827295c4356d1ba72dbb9
The Dragon Lord <The Dragon Lord@172.24.48.253>
2018-04-24 18:04:43 -0500

    Hi

--- src/application/model/Wardrobe.java
+++ src/application/model/Wardrobe.java
@@ -12,31 +12,49 @@ import java.util.Scanner;
  */
 public class Wardrobe {
 	
+	private String name, imageFileName;
+	private boolean[] temperature = new boolean[4];
+	
+	
+	public Wardrobe(String name) {
+		this.name = name;
+	}
+	
 	/**
 	 * Wardrobe constructor
 	 * 
 	 * @param cT
 	 */
-	public Wardrobe(String cT) {
-	}
-	
-	Scanner userInput = new Scanner(System.in);
-	
-	public void addClothing() {
-		
-		System.out.println("enter brand here");
-		String brand = userInput.nextLine();
-		
-		System.out.println("Enter color here");
-		String color = userInput.nextLine();
-		
-		System.out.println("Enter style here");
-		String style = userInput.nextLine();
-		
-		System.out.println(brand + "\n" + color + "\n" + style);
-		userInput.close();
+	public Wardrobe(String name, String image) {
+		this.name = name;
+		this.imageFileName = image;
 	}
 
+	
+	//Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImageFileName() {
+		return imageFileName;
+	}
+
+	public void setImageFileName(String imageFileName) {
+		this.imageFileName = imageFileName;
+	}
+
+	public boolean[] getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(boolean[] temperature) {
+		this.temperature = temperature;
+	}
 	
 	
 }
