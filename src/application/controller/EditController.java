bf9c3623aa26576c89e6b6bf5aed7c224ee9bd56
zzn974 <zzn974@N0000A114.cs.utsarr.net>
2018-04-26 15:22:13 -0500

    Edit

---
+++ src/application/controller/EditController.java
@@ -0,0 +1,29 @@
+package application.controller;
+
+import application.model.Wardrobe;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class EditController 
+{
+	@FXML
+	private ListView<Wardrobe> tops;
+	@FXML
+	private ListView<Wardrobe> bottoms;
+	@FXML
+	private ListView<Wardrobe> shoes;
+	@FXML
+	private Button edit;
+	@FXML
+	private Button home;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
